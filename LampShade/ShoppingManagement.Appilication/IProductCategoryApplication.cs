2bd9ae4d2df0282a935ae554760eaa19f432c193
kalami-f <kalami-f@ICTB01F08C9019>
2023-09-03 16:03:34 +0330

    Add ProductCategory initial

---
+++ LampShade/ShoppingManagement.Appilication/IProductCategoryApplication.cs
@@ -0,0 +1,40 @@
+﻿using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagement.Appilication
+{
+    public class ProductCategoryApplication : IProductCategoryAppilcation
+    {
+        private readonly IProductCategoryRepository _productCategoryRepository;
+
+        public ProductCategoryApplication(IProductCategoryRepository productCategoryRepository)
+        {
+            _productCategoryRepository = productCategoryRepository;
+        }
+
+        public void Create(CreateProductCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(EditProductCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
