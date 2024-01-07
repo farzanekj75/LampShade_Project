2deaa6bb7f91901521299f39a495851d76691d9d
kalami-f <kalami-f@ICTB01F08C9019>
2024-01-07 16:15:10 +0330

    20-part1-02

---
+++ LampShade/AccountManagement.Infrastructure.EFCore/AccountContext.cs
@@ -0,0 +1,22 @@
+﻿using AccountManagement.Domain.AccountAgg;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AccountManagement.Infrastructure.EFCore
+{
+    public class AccountContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public AccountContext(DbContextOptions<AccountContext> options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(AccountContext).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
