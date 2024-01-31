47fd655ead0162ef4fbe4348096326547001ba29
kalami-f <kalami-f@ICTB01F08C9019>
2024-01-31 14:16:53 +0330

    21-part3

---
+++ LampShade/ServiseHost/wwwroot/Theme/assets/js/Custom.js
@@ -0,0 +1,64 @@
+﻿function addToCart(id, name, price, picture) {
+    let products = $.cookie("cart-items");
+    if (products === undefined) {
+        products = [];
+    } else {
+        products = JSON.parse(products);
+    }
+
+    const count = $("#productCount").val();
+    const currentProduct = products.find(x => x.id === id);
+    if (currentProduct !== undefined) {
+        products.find(x => x.id === id).count = parseInt(currentProduct.count) + parseInt(count);
+    } else {
+        const product = {
+            id,
+            name,
+            price,
+            picture,
+            count
+        }
+
+        products.push(product);
+    }
+    $.cookie("card-items", JSON.stringify(products), { expires: 2, path: "/" })
+    updateCart();
+}
+
+function updateCart() {
+    let products = $.cookie(cookieName);
+    products = JSON.parse(products);
+    $("#cart_items_count").text(products.length);
+    const cartItemsWrapper = $("#cart_items_wrapper");
+    cartItemsWrapper.html('');
+    products.forEach(x => {
+        const product = ` <div class="single-cart-item">
+            < a href = "javascript:void(0)" class="remove-icon" onclick="removeFromCart('${x.id}')" >
+                <i class="ion-android-close"></i>
+                              </a >
+                              <div class="image">
+                                <a href="single-product.html">
+                                    <img src="~/Theme/assets/img/products/product-1-80x106.jpg"
+                                        class="img-fluid" alt="">
+                                </a>
+                               </div>
+                              <kdiv class="content">
+                                 <p class="product-title">
+                                     <a href="single-product.html">محصول : ${x.name}</a>
+                                 </p>
+                                 <p class="count">تعداد: ${x.count}</p>
+                                 <p class="count">قیمت واحد: ${x.price}</p>
+                              </div>
+                           </div >` ;
+        cartItemsWrapper.append(product);
+
+    });
+}
+function removeFromCart(id) {
+    let products = $.cookie(cookieName);
+    products = JSON.parse(products);
+    const itemToRemove = products.findIndex(x => x.id === id);
+    products.splice(itemToRemove, 1);
+    $.cookie(cookieName, JSON.stringify(products), { expires: 2, path: "/" });
+    updateCart();
+}
